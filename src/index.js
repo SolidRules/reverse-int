a0160d0da442ef68b05eecd18803e97f1c302c98
SolidRules <amikheev1993@gmail.comm>
2022-06-28 00:18:59 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let str = n.toString();
+        
+    for (let i = 0; i < str.length; i++) { 
+        if (str[i] === '-') {
+            i += 1;
+    } result = `${str[i]}${result}`;
+}
+    return result;
 }
